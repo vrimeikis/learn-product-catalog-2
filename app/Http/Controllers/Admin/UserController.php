3c5dfc92b53f9e2264fe8f6bd32238a46a337f52
paulius5 <piligrimaz@gmail.com>
2018-09-05 19:14:45 +0300

    Users list.

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\Repositories\UserRepository;
+use App\User;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class UserController
+ * @package App\Http\Controllers\Admin
+ */
+class UserController extends Controller
+{
+    /** @var UserRepository */
+    private $userRepository;
+
+    /**
+     * UserController constructor.
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function index(): View
+    {
+        $users = $this->userRepository->all();
+
+        return view('admin.user.list', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/user/list.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Users list</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <table class="table">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                                <th>Actions</th>
+                            </tr>
+
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->last_name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
