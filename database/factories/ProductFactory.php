8c0925cc8359f77a30dae74579da3e238476f9e3
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2018-09-05 19:20:17 +0300

    Product faker added

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Product;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Product::class, function(Faker $faker) {
+
+    $title = $faker->unique()->sentence(6);
+
+    return [
+        'title' => $title,
+        'cover' => 'articles/' . $faker->file(
+                resource_path('img'),
+                storage_path('app/public/products'),
+                false
+            ),
+        'context' => $faker->paragraph,
+        'slug' => Str::slug($title),
+        'price' => $faker->numberBetween(10.00, 1000.00),
+        'active' => (bool)random_int(0, 1),
+
+    ];
+});
