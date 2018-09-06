1e842da1f6b45ac1e872a66e7923359ee00a81cf
paulius5 <piligrimaz@gmail.com>
2018-09-06 21:02:29 +0300

    Removed users dropdown list.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,18 +49,9 @@
                             </li>
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link" href="{{ route('admin.users.index') }}" role="button" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ __('Users') }} <span class="caret"></span>
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('admin.users.index') }}">
-                                        {{ __('List') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('admin.users.create') }}">
-                                        {{ __('Create') }}
-                                    </a>
-                                </div>
                             </li>
 
                             <li class="nav-item dropdown">
